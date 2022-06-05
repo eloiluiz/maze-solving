e004ace049d55d082339b3209ab93565c08dbd85
Eloi Filho <eloi.filho@electrolux.com>
2022-06-05 15:37:13 -0300

    Dijkstra search method initial design;

--- maze-solving.py
+++ maze-solving.py
@@ -20,7 +20,7 @@ __status__ = "Development"
 #                    Configuration Parameters
 # **************************************************************
 PRINT_INFO = False
-PRINT_DEBUG = False
+PRINT_DEBUG = True
 
 # **************************************************************
 #                           Libraries
@@ -281,13 +281,13 @@ class Maze:
         elif (self.__map[y, x] == self.MARKED_GOAL_INDEX):
             self.__map[y, x] = self.SELECTED_GOAL_INDEX
         elif (self.__map[y, x] == self.HALL_INDEX):
-            self.__map[y, x] = self.MARKED_HALL_INDEX
+            self.__map[y, x] = self.SELECTED_HALL_INDEX
         elif (self.__map[y, x] == self.WALL_INDEX):
-            self.__map[y, x] = self.MARKED_WALL_INDEX
+            self.__map[y, x] = self.SELECTED_WALL_INDEX
         elif (self.__map[y, x] == self.START_INDEX):
-            self.__map[y, x] = self.MARKED_START_INDEX
+            self.__map[y, x] = self.SELECTED_START_INDEX
         elif (self.__map[y, x] == self.GOAL_INDEX):
-            self.__map[y, x] = self.MARKED_GOAL_INDEX
+            self.__map[y, x] = self.SELECTED_GOAL_INDEX
 
     def set_path(self, coordinates=[]):
         """Mark the input coordinates in the map.
@@ -812,16 +812,16 @@ class AgentSearchNode:
         return (self.rank >= other.rank)
 
 
-class AStarAgent(Agent):
-    """A* Search Method
+class DijkstraAgent(Agent):
+    """Dijkstra Search Method
 
-    This class implements the A* Search algorithm.
+    This class implements the Dijkstra Search algorithm.
 
     Args:
         Agent (object): The search algorithm intelligent agent parent class.
 
     Returns:
-        object: The A* agent object.
+        object: The Dijkstra agent object.
     """
 
     def __init__(self, maze, return_first=True, break_wall=0):
@@ -834,11 +834,16 @@ class AStarAgent(Agent):
         self._open = queue.PriorityQueue()
         self._closed = []
         self._return_first = return_first
-        # Execute the search
-        start_node = AgentSearchNode(0, 0, self.f(self._start_position), self._start_position, break_wall)
-        self.search(start_node)
+        self._break_wall = break_wall
 
-    def movement_cost(self, origin=[], destination=[]):
+    def start(self):
+        """Method that starts the goal search process and returns the resulting path.
+        """
+        # Execute the search
+        start_node = AgentSearchNode(0, 0, self._f(self._start_position), self._start_position, self._break_wall)
+        return self._search(start_node)
+
+    def _movement_cost(self, origin=[], destination=[]):
         """Agent heuristic function that calculates movement costs.
 
         This algorithm uses the Manhattan distance to calculates movement cost as this is the standard heuristic for a
@@ -855,22 +860,7 @@ class AStarAgent(Agent):
         dx = abs(destination[1] - origin[1])
         return (dx + dy)
 
-    def g(self, coordinates=[]):
-        """Agent heuristic function that calculates movement costs from the start position.
-
-        This is a heuristic function that represents the exact cost of the path from the start position to the input 
-        coordinate values. This algorithm uses the Manhattan distance as this is the standard heuristic for a square 
-        grid.
-
-        Args:
-            coordinates (list): The movement coordinates [x, y].
-
-        Returns:
-            int: The movement cost estimation.
-        """
-        return self.movement_cost(self._start_position, coordinates)
-
-    def h(self, coordinates=[]):
+    def _h(self, coordinates=[]):
         """Agent heuristic function the calculates movement costs to the goal position.
 
         This is a heuristic function that estimates the cost of the cheapest path from the input coordinate values to 
@@ -882,9 +872,9 @@ class AStarAgent(Agent):
         Returns:
             int: The movement cost estimation.
         """
-        return self.movement_cost(coordinates, self._goal_position)
+        return self._movement_cost(coordinates, self._goal_position)
 
-    def f(self, coordinates=[]):
+    def _f(self, coordinates=[]):
         """Agent heuristic function the calculates the total cost of the selected coordinate.
 
         This is a heuristic function that estimates the total movement cost of the input coordinate. This algorithm uses
@@ -896,9 +886,9 @@ class AStarAgent(Agent):
         Returns:
             int: The movement cost estimation.
         """
-        return (self.g(coordinates) + self.h(coordinates))
+        return self._h(coordinates)
 
-    def search(self, node):
+    def _search(self, node):
         """Agent search method.
 
         Args:
@@ -923,13 +913,13 @@ class AStarAgent(Agent):
             # Put the visited node to the closed list
             self._closed.append(current_node)
 
-            # Print current movement step
-            if (PRINT_DEBUG == True):
-                self._maze.mark_position(current_position[0], current_position[1])
-                self._maze.print_map()
-                print("Current position = ", current_position)
-                input("PRESS ANY KEY TO CONTINUE...")
-                self._maze.clear_path()
+            # # Print current movement step
+            # if (PRINT_DEBUG == True):
+            #     self._maze.mark_position(current_position[0], current_position[1])
+            #     self._maze.print_map()
+            #     print("Current position = ", current_position)
+            #     input("PRESS ANY KEY TO CONTINUE...")
+            #     self._maze.clear_path()
 
             # Test for goal position
             # If True, store the path found
@@ -967,8 +957,8 @@ class AStarAgent(Agent):
                     continue
 
                 # Calculate the neighbor costs (from current and from start position)
-                neighbor_new_cost = current_node.cost + self.movement_cost(current_position, neighbor_position)
-                neighbor_gcost = self.g(neighbor_position)
+                neighbor_new_cost = current_node.cost + self._movement_cost(current_position, neighbor_position)
+                neighbor_gcost = self._f(neighbor_position)
 
                 # Check if the neighbor is at any of the open and closed lists
                 is_neighbor_open = False
@@ -996,9 +986,228 @@ class AStarAgent(Agent):
 
                 # If neightbor is not in any of the lists, add it to open
                 if ((is_neighbor_open == False) and (is_neighbor_closed == False)):
-                    neighbor_node = AgentSearchNode(current_node, neighbor_new_cost + self.f(neighbor_position),
+                    neighbor_node = AgentSearchNode(current_node, neighbor_new_cost + self._f(neighbor_position),
                                                     neighbor_new_cost, neighbor_position, current_node.break_wall)
                     self._open.put(neighbor_node)
+
+            # Print current search
+            if (PRINT_DEBUG == True):
+                for closed_node in self._closed:
+                    self._maze.mark_position(closed_node.position[0], closed_node.position[1])
+                for open_node in self._open.queue:
+                    self._maze.select_position(open_node.position[0], open_node.position[1])
+                self._maze.select_position(current_position[0], current_position[1])
+                self._maze.print_map()
+                input()
+                self._maze.clear_path()
+
+        # If the open list gets empty, the goal was not found
+        return False
+
+
+class AStarAgent(Agent):
+    """A* Search Method
+
+    This class implements the A* Search algorithm.
+
+    Args:
+        Agent (object): The search algorithm intelligent agent parent class.
+
+    Returns:
+        object: The A* agent object.
+    """
+
+    def __init__(self, maze, return_first=True, break_wall=0):
+        """Initialize the search agent and execute.
+        """
+        # Initialization process
+        Agent.__init__(self, maze)
+        self._path = []
+        self._goal_position = self._maze.get_goal_position()
+        self._open = queue.PriorityQueue()
+        self._closed = []
+        self._return_first = return_first
+        self._break_wall = break_wall
+
+    def start(self):
+        """Method that starts the goal search process and returns the resulting path.
+        """
+        # Execute the search
+        start_node = AgentSearchNode(0, 0, self._f(self._start_position), self._start_position, self._break_wall)
+        return self._search(start_node)
+
+    def _movement_cost(self, origin=[], destination=[]):
+        """Agent heuristic function that calculates movement costs.
+
+        This algorithm uses the Manhattan distance to calculates movement cost as this is the standard heuristic for a
+        square grid.
+
+        Args:
+            origin (list): The origin (start) position coordinates [y, x].
+            destination (list): The destination (end) position coordinates [y, x].
+
+        Returns:
+            int: The movement cost estimation.
+        """
+        dy = abs(destination[0] - origin[0])
+        dx = abs(destination[1] - origin[1])
+        return (dx + dy)
+
+    def _g(self, coordinates=[]):
+        """Agent heuristic function that calculates movement costs from the start position.
+
+        This is a heuristic function that represents the exact cost of the path from the start position to the input 
+        coordinate values. This algorithm uses the Manhattan distance as this is the standard heuristic for a square 
+        grid.
+
+        Args:
+            coordinates (list): The movement coordinates [x, y].
+
+        Returns:
+            int: The movement cost estimation.
+        """
+        return self._movement_cost(self._start_position, coordinates)
+
+    def _h(self, coordinates=[]):
+        """Agent heuristic function the calculates movement costs to the goal position.
+
+        This is a heuristic function that estimates the cost of the cheapest path from the input coordinate values to 
+        the goal. This algorithm uses the Manhattan distance as this is the standard heuristic for a square grid.
+
+        Args:
+            coordinates (list): The movement coordinates [y, x].
+
+        Returns:
+            int: The movement cost estimation.
+        """
+        return self._movement_cost(coordinates, self._goal_position)
+
+    def _f(self, coordinates=[]):
+        """Agent heuristic function the calculates the total cost of the selected coordinate.
+
+        This is a heuristic function that estimates the total movement cost of the input coordinate. This algorithm uses
+        the Manhattan distance as this is the standard heuristic for a square grid.
+
+        Args:
+            coordinates (list): The movement coordinates [y, x].
+
+        Returns:
+            int: The movement cost estimation.
+        """
+        return (self._g(coordinates) + self._h(coordinates))
+
+    def _search(self, node):
+        """Agent search method.
+
+        Args:
+            node (AgentSearchNode): current A* node used in the search process.
+
+        Returns:
+            bool: The movement result, where True means the goal position is reached and False that it hasn't.
+        """
+
+        # Rename the input node
+        current_node = node
+        # Update the visited positions list
+        self._open.put(current_node)
+
+        # Iterate over the open queue
+        while (not self._open.empty()):
+
+            # Remove the lowest ranking node from the queue
+            current_node = self._open.get()
+            current_position = current_node.position
+
+            # Put the visited node to the closed list
+            self._closed.append(current_node)
+
+            # # Print current movement step
+            # if (PRINT_DEBUG == True):
+            #     self._maze.mark_position(current_position[0], current_position[1])
+            #     self._maze.print_map()
+            #     print("Current position = ", current_position)
+            #     input("PRESS ANY KEY TO CONTINUE...")
+            #     self._maze.clear_path()
+
+            # Test for goal position
+            # If True, store the path found
+            if (self.is_goal_position(current_position[0], current_position[1])):
+                new_path = []
+                while (current_node != 0):
+                    new_path.append(current_node.position)
+                    current_node = current_node.parent
+                new_path_length = len(new_path)
+                # Check if shortest path
+                if ((self._path_length == 0) or (new_path_length < self._path_length)):
+                    self._path = new_path
+                    self._path_length = new_path_length
+                # Check if the search must continue
+                if (self._return_first == True):
+                    return True
+                else:
+                    continue
+
+            # If current position isn't the goal, search it's neighbors
+            wall_broken = False
+            for neighbor_position in self._maze.get_neighbors(current_position):
+
+                # Reset last iteration wall break counter
+                if (wall_broken == True):
+                    current_node.break_wall += 1
+                    wall_broken = False
+
+                # First, check if the neighbor is a valid position (open path)
+                neighbor_position_value = self._maze.get_position_value(neighbor_position[0], neighbor_position[1])
+                if (current_node.break_wall > 0):
+                    current_node.break_wall -= 1
+                    wall_broken = True
+                elif (neighbor_position_value == 1):
+                    continue
+
+                # Calculate the neighbor costs (from current and from start position)
+                neighbor_new_cost = current_node.cost + self._movement_cost(current_position, neighbor_position)
+                neighbor_gcost = self._g(neighbor_position)
+
+                # Check if the neighbor is at any of the open and closed lists
+                is_neighbor_open = False
+                is_neighbor_closed = False
+
+                # Check if neighbor is at the open list and is a viable path
+                for open_node in self._open.queue:
+                    open_position = open_node.position
+                    if (neighbor_position == open_position):
+                        if (neighbor_new_cost < neighbor_gcost):
+                            # Remove position from the open queue
+                            self._open.queue.remove(open_node)
+                        else:
+                            is_neighbor_open = True
+
+                # Check if neighbor is at the closed list and is a viable path
+                for closed_node in self._closed:
+                    closed_position = closed_node.position
+                    if (neighbor_position == closed_position):
+                        if (neighbor_new_cost < closed_node.cost):
+                            # Remove position from the closed list
+                            self._closed.remove(closed_node)
+                        else:
+                            is_neighbor_closed = True
+
+                # If neightbor is not in any of the lists, add it to open
+                if ((is_neighbor_open == False) and (is_neighbor_closed == False)):
+                    neighbor_node = AgentSearchNode(current_node, neighbor_new_cost + self._f(neighbor_position),
+                                                    neighbor_new_cost, neighbor_position, current_node.break_wall)
+                    self._open.put(neighbor_node)
+
+            # Print current search
+            if (PRINT_DEBUG == True):
+                for closed_node in self._closed:
+                    self._maze.mark_position(closed_node.position[0], closed_node.position[1])
+                for open_node in self._open.queue:
+                    self._maze.select_position(open_node.position[0], open_node.position[1])
+                self._maze.select_position(current_position[0], current_position[1])
+                self._maze.print_map()
+                input()
+                self._maze.clear_path()
 
         # If the open list gets empty, the goal was not found
         return False
@@ -1011,30 +1220,40 @@ if __name__ == "__main__":
     # Create a randomized map and print
     maze = Maze(50, 10, .75, .75)
     maze.print_map()
+    print("\n")
 
-    # Solve the maze using the BFS algorithm
-    bfs_agent = BFS_Search(maze)
-    print("\n BFS Search:")
-    maze.set_path(bfs_agent.get_path())
-    maze.print_map()
-    maze.clear_path()
+    # # Solve the maze using the BFS algorithm
+    # bfs_agent = BFS_Search(maze)
+    # print("\n BFS Search:")
+    # maze.set_path(bfs_agent.get_path())
+    # maze.print_map()
+    # maze.clear_path()
 
-    # Solve the maze using the DFS algorithm
-    dfs_agent = DFS_Search(maze)
-    maze.set_path(dfs_agent.get_path())
-    print("\n DFS Search:")
-    maze.print_map()
-    maze.clear_path()
+    # # Solve the maze using the DFS algorithm
+    # dfs_agent = DFS_Search(maze)
+    # maze.set_path(dfs_agent.get_path())
+    # print("\n DFS Search:")
+    # maze.print_map()
+    # maze.clear_path()
 
-    # Solve the maze using the IDFS algorithm
-    idfs_agent = IDFS_Search(maze)
-    print("\n IDFS Search:")
-    maze.set_path(idfs_agent.get_path())
-    maze.print_map()
-    maze.clear_path()
+    # # Solve the maze using the IDFS algorithm
+    # idfs_agent = IDFS_Search(maze)
+    # print("\n IDFS Search:")
+    # maze.set_path(idfs_agent.get_path())
+    # maze.print_map()
+    # maze.clear_path()
 
-    # Solve the maze using the A* algorithm
+    # # Solve the maze using the Dijkstra algorithm
+    # dj_agent = DijkstraAgent(maze, return_first=False, break_wall=1)
+    # dj_agent.start()
+    # print("\n Dijkstra - Dijkstra Search:")
+    # maze.set_path(dj_agent.get_path())
+    # maze.print_map()
+    # maze.clear_path()
+
+    # Solve the maze using the Dijkstra algorithm
     as_agent = AStarAgent(maze, return_first=False, break_wall=1)
+    as_agent.start()
     print("\n A* - A* Search:")
     maze.set_path(as_agent.get_path())
     maze.print_map()
@@ -1042,11 +1261,12 @@ if __name__ == "__main__":
 
     print("\n Search Summary")
     summary = queue.PriorityQueue()
-    summary.put((len(bfs_agent.get_path()), "BFS:"))
-    summary.put((len(dfs_agent.get_path()), "DFS:"))
-    summary.put((len(idfs_agent.get_path()), "IDFS:"))
-    summary.put((len(as_agent.get_path()), "A*:"))
+    # summary.put((len(bfs_agent.get_path()), "BFS:     "))
+    # summary.put((len(dfs_agent.get_path()), "DFS:     "))
+    # summary.put((len(idfs_agent.get_path()), "IDFS:    "))
+    # summary.put((len(dj_agent.get_path()), "Dijkstra:"))
+    summary.put((len(as_agent.get_path()), "A*:      "))
 
     while (not summary.empty()):
         length, name = summary.get()
-        print(" " + name + "\t" + str(length))
+        print(" " + name + " " + str(length))
